c763762957f21c7d2d9f1d34f2746d1543ff9514
dredko25 <dredko25@gmail.com>
2026-03-17 12:24:00 +0200

    add OnModelCreating

--- ECommerce_Project.DataAccess/ECommerceDbContext.cs
+++ ECommerce_Project.DataAccess/ECommerceDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ECommerce_Project.DataAccess.Models;
+using ECommerce_Project.DataAccess.Configurations;
 
 namespace ECommerce_Project.DataAccess;
 
-public class ECommerceDbContext : DbContext
+public class ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : DbContext(options)
 {
     public DbSet<ProductEntity> Products { get; set; } = null!;
     public DbSet<UserEntity> Users { get; set; } = null!;
@@ -12,5 +13,18 @@ public class ECommerceDbContext : DbContext
     public DbSet<CartItemEntity> CartItems { get; set; } = null!;
     public DbSet<OrderEntity> Orders { get; set; } = null!;
     public DbSet<OrderItemEntity> OrderItems { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new OrderItemConfiguration());
+        modelBuilder.ApplyConfiguration(new OrderConfiguration());
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new CartConfiguration());
+        modelBuilder.ApplyConfiguration(new CartItemConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
 
 }
