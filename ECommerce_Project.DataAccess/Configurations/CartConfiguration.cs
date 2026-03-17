c3506a5e71a8d962db2fedecd4f7a4c0c4b786a3
dredko25 <dredko25@gmail.com>
2026-03-17 12:14:12 +0200

    add CartConfiguration

---
+++ ECommerce_Project.DataAccess/Configurations/CartConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using ECommerce_Project.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce_Project.DataAccess.Configurations;
+
+public class CartConfiguration : IEntityTypeConfiguration<CartEntity>
+{
+    public void Configure(EntityTypeBuilder<CartEntity> builder)
+    {
+        builder.HasKey(c => c.Id);
+
+        builder.HasOne(c => c.User)
+               .WithOne(u => u.Cart)
+               .HasForeignKey<CartEntity>(c => c.UserId)
+               .IsRequired()
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(c => c.CreatedAt)
+               .IsRequired();
+
+        builder.HasMany(c => c.CartItems)
+               .WithOne(ci => ci.Cart)
+               .HasForeignKey(ci => ci.CartId)
+               .IsRequired()
+               .OnDelete(DeleteBehavior.Cascade);
+
+    }
+}
